baec722670e333c682d98a20318a6dc8918d4d5f
jbeemster <joshua.a.beemster@gmail.com>
2025-01-03 15:41:16 +0100

    Update to use https_traffic_only_enabled

--- main.tf
+++ main.tf
@@ -3,14 +3,14 @@ data "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage_account" {
-  name                      = var.name
-  resource_group_name       = var.resource_group_name
-  location                  = data.azurerm_resource_group.rg.location
-  account_tier              = var.sku
-  account_replication_type  = var.replication_type
-  account_kind              = var.kind
-  enable_https_traffic_only = true
-  min_tls_version           = "TLS1_2"
+  name                       = var.name
+  resource_group_name        = var.resource_group_name
+  location                   = data.azurerm_resource_group.rg.location
+  account_tier               = var.sku
+  account_replication_type   = var.replication_type
+  account_kind               = var.kind
+  https_traffic_only_enabled = true
+  min_tls_version            = "TLS1_2"
 
   is_hns_enabled = var.is_hns_enabled
 
